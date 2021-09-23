e151588d73dc22425fa42570492101b2d2d6cf9a
Nelson Berrio <nberrio1996@gmail.com>
2021-09-23 16:48:34 -0500

    Se agrega el codigo html base para la pagina del login

---
+++ src/pages/login.jsx
@@ -0,0 +1,23 @@
+import logo from 'media/logo.png'
+
+const Login = () => {
+    return(
+        <>
+            <div className="container">
+                <div className="login-container">
+                    <div className="login-items">
+                        <h2>Iniciar sesión</h2>
+                        <form action="">
+                            <img src={logo} alt="" />
+                            <input type="text" placeholder="Usuario"/>
+                            <input type="text" placeholder="Contraseña"/>
+                            <input type="submit" value="INGRESAR" id="login-btn"/>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Login;

4f9951987ff7be8c68977bb3be2ad423f7a2aa13
LorenaDoria <lorenadoria0426@gmail.com>
2021-09-29 18:34:06 -0500

    creando la rama del sidebar

---
+++ src/Layouts/Sidebar.jsx
@@ -0,0 +1,44 @@
+import { NavLink } from 'react-router-dom'
+import logo from './logo.png';
+import * as FaIcons from 'react-icons/fa'
+function Sidebar(){
+    return(
+        <div className="sidebar">  
+        <img className="logo" src={logo} />
+    <ul>
+    <hr></hr>
+        <li>
+            <NavLink to="/" exact className="mr-2 rounded py-1 w-100 d-inline-block px-1 text-white " 
+            activeClassName="active"><FaIcons.FaHome/> Inicio</NavLink>
+        </li>
+        <li>
+            <NavLink to="/registrarVentas" exact className="mr-2 text-white rounded py-1 w-100 d-inline-block px-1" 
+            activeClassName="active"><FaIcons.FaShoppingCart/> Registro de Ventas</NavLink>
+        </li>
+        <li>
+            <NavLink to="/gestionVentas" exact className="mr-2 text-white rounded py-1 w-100 d-inline-block px-1" 
+            activeClassName="active"><FaIcons.FaRegCalendar/> Gestión de Ventas</NavLink>
+        </li>
+        <li>
+            <NavLink to="/registroProductoNuevo" exact className="mr-2 text-white rounded py-1 w-100 d-inline-block px-1"
+             activeClassName="active"><FaIcons.FaThLarge/> Registro de Productos</NavLink>
+        </li>
+        <li>
+            <NavLink to="/registroProducto" exact className="mr-2 text-white rounded py-1 w-100 d-inline-block px-1"
+             activeClassName="active"><FaIcons.FaShoppingBag/> Gestión de Productos</NavLink>
+        </li>
+        <li>
+            <NavLink to="/gestionUsuarios" exact className="mr-2 text-white rounded py-1 w-100 d-inline-block px-1"
+             activeClassName="active"><FaIcons.FaUserCog/> Gestión de Usuarios</NavLink>
+        </li>
+        <hr id="hr-final"></hr>
+        <li>
+            <NavLink id="logout" to="/logout" exact className="mr-2 text-white rounded py-1 w-100 d-inline-block px-1"
+             activeClassName="active"><FaIcons.FaSignOutAlt/> Cerrar Sesión</NavLink>
+        </li>
+     
+    </ul>
+        </div>
+    )
+}
+export default Sidebar

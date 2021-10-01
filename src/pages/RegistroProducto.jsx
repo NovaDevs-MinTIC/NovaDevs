ceb097631d5c1e80e469bdecc682ab23ed4f2377
LorenaDoria <lorenadoria0426@gmail.com>
2021-10-01 16:16:05 -0500

    editando el archivo scss

--- src/pages/RegistroProducto.jsx
+++ src/pages/RegistroProducto.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment,useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+//import 'react-toastify/dist/ReactToastify.css';
 
 const RegistroProductos=()=>{
 

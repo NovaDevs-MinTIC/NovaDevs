671b50bd0b7c10febeeba0f8c32ee737e797637a
Oscar <os.gomez.lozano@gmail.com>
2021-10-02 16:42:40 -0500

    Div para contener la página de buscar y editar

--- src/pages/BuscarActualizarVentas.jsx
+++ src/pages/BuscarActualizarVentas.jsx
@@ -5,34 +5,38 @@ import DataVentas from 'components/DataVentas'
 
 const BuscarActualizarVentas = () => {
     return (
-        <div className="container-sm container-fluid">
-                {/*Título*/}
-                <div className="container-sm container-fluid">
-                    <h1 className="h1__titBuscarActulizarVentas mt-2">
-                        Estado de venta
-                    </h1>
-                </div>
+        <div className="contenedorVentas">
+            
+        <div className="container">
 
-                {/*Barras de búsqueda para la tabla (Cuestionandome donde debería ser posicionado*/}
-                <div className="container-sm container-fluid">
-                    <div className="input-group mb-3">
-                        <Buscadores texto="Id de la venta" />
-                        <Buscadores texto="Id del cliente" />    
-                        <button type="button align-right" className="btn btn-light mb-3 ml-3 col col inline " aria-pressed="false">Buscar Venta</button>
-                    </div>
-                </div>
+            {/*Título*/}
+            <div className="container">
+                <h1 className="h1__titBuscarActulizarVentas mt-2">
+                    Estado de venta
+                </h1>
+            </div>
 
-                {/*Contenedor de la tabla de Buscar y actualizar venta*/}
-                <div className="container-sm container-fluid">
-                    <div className="table-responsive">
-                        <DataVentas />
-                    </div>
+            {/*Barras de búsqueda para la tabla (Cuestionandome donde debería ser posicionado*/}
+            <div className="container">
+                <div className="input-group mb-3">
+                    <Buscadores texto="Id de la venta" />
+                    <Buscadores texto="Id del cliente" />    
+                    <button type="button align-right" className="btn btn-light mb-3 ml-3 col col inline " aria-pressed="false">Buscar Venta</button>
                 </div>
-                
-                {/*Botón Para regresar*/}
-                <div className="container-sm container-fluid mt-3 mb-3 d-flex flex-row-reverse">
-                    <button type="button" className="btn">Regresar</button>
+            </div>
+
+            {/*Contenedor de la tabla de Buscar y actualizar venta*/}
+            <div className="container">
+                <div className="table-responsive">
+                    <DataVentas />
                 </div>
+            </div>
+            
+            {/*Botón Para regresar*/}
+            <div className="container mt-3 mb-3 d-flex flex-row-reverse">
+                <button type="button" className="btn">Regresar</button>
+            </div>
+        </div>
         </div>
     )
 }

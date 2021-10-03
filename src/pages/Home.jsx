913da43da309b8528b1e68388e2936cbfbddd53e
LorenaDoria <lorenadoria0426@gmail.com>
2021-10-02 19:51:52 -0500

    cambiando el bienvenido

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Logo from 'media/logo.png'
-
+import 'styles/Home.css';
 
 const Home = () => {
     return (
             <div className="container-home">
                 <div className="home-logo">
                <img src={Logo} alt="" className="logo-home"/>
-               <h2 ClassName="bienvenido">Bienvendo</h2>
+               <h2 ClassName="bienvenido">Bienvenido</h2>
                </div>
             </div>        
       );

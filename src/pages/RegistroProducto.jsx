772266784b03df27bb9876d86a49e7ecd76ebbd1
Nelson Berrio <nberrio1996@gmail.com>
2021-10-07 14:32:51 -0500

    Se modifico pagina de registro de productos agregando solamente clases de tailwind

--- src/pages/RegistroProducto.jsx
+++ src/pages/RegistroProducto.jsx
@@ -3,19 +3,24 @@ import React  from 'react'
 const RegistroProductos=()=>{
 
     return(
-      <div>
-        <div className='max-w-md w-screen space-y-8'>
-          <h2 className="text-3xl uppercase font-extrabold text-gray-900 text-center mt-4 mb-2">Registro de producto</h2>
-          <form className='mt-8 space-y-3'>
-            <div className='rounded-md shadow-sm -space-y-px'>
+      <div className='h-full w-full'>
+        {/* TITULO */}
+        <div className='flex items-center justify-center w-full h-1/6'>
+            <h2 className='text-4xl font-extrabold text-gray-900'>
+            REGISTRO DE PRODUCTOS
+            </h2>
+        </div>
+
+        {/* FORMULARIO DE REGISTRO DE PRODUCTO */}
+        <div className="w-full h-1/2 flex flex-wrap justify-center items-center">
+          <form>
               <div>
-                  <label classsName='mx-2'>Identificador de producto</label>
+                  <label>Identificador de producto</label>
                   <input
-                          name='ID-Producto'
+                          name='idproducto'
                           type='number'
-                          autoComplete='number'
                           required
-                          className='appearance-none rounded-md relative block w-full mb-2 px-3 py-2 border border-novablue placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                          className='appearance-none rounded-md relative w-full mb-2 px-3 py-2 border border-novablue placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
                           placeholder='ID Producto'
                         />
               </div>
@@ -53,23 +58,19 @@ const RegistroProductos=()=>{
                   <option value="2">No disponible</option>
                 </select>
               </div>
-            </div>
           </form>
         </div>
-
-        <div className='max-w-md w-full'>
-          <div>
+        <div className="flex justify-center mt-6">
             <button
-              type='submit'
-              className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-novablue hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500'
+            type='submit'
+            className='group relative w-auto flex py-2 px-2 border border-transparent text-sm font-medium rounded-md text-white bg-novablue hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500'
             >
-              <div className='flex items-center justify-start'>
-                <span className='mx-4'>Registrar</span>
-              </div>
+            <div className='flex items-center'>
+                <i className="fas fa-plus"></i>
+                <span className='mx-2'>Registrar</span>
+            </div>
             </button>
-          </div>
         </div>
-
       </div>        
     )
 }

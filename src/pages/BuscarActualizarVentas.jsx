d95bf93c8fd7843f42149ff99acdc06df3b259e0
Nelson Berrio <nberrio1996@gmail.com>
2021-10-07 15:02:03 -0500

    Se configuro la pagina de gestion de productos con clases de tailwind

--- src/pages/BuscarActualizarVentas.jsx
+++ src/pages/BuscarActualizarVentas.jsx
@@ -27,25 +27,55 @@ const BuscarActualizarVentas = () => {
             </div>
 
             {/* NUEVA TABLA DE VENTAS */}
-            
-            <div className="w-full flex flex-wrap h-4/6 overflow-y-scroll">
-                <table className="tabla">
-                    <thead>
-                        <tr>
-                            <th className="text-center">ID Venta</th>
-                            <th className="text-center">Descripción de venta</th>
-                            <th className="text-center">Fecha Venta</th>
-                            <th className="text-center">Valor Venta</th> 
-                            <th className="text-center">Estado Venta</th>
-                            <th className="text-center">Cliente</th>
-                            <th className="text-center">Vendedor</th>                                
-                            <th className="text-center">Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                    </tbody>
-                </table>
+            <div className="w-full h-4/6">
+                <div className="w-full overflow-x-scroll">
+                    <table class="tabla mt-4">
+                        <thead>
+                            <tr>
+                                <th className="text-center">ID Venta</th>
+                                <th className="text-center">Descripción de venta</th>
+                                <th className="text-center">Fecha Venta</th>
+                                <th className="text-center">Valor Venta</th> 
+                                <th className="text-center">Estado Venta</th>
+                                <th className="text-center">Cliente</th>
+                                <th className="text-center">Vendedor</th>                                
+                                <th className="text-center">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>00123</td>
+                                <td>Adidas Stan Smith</td>
+                                <td>12</td>
+                                <td>229900</td>
+                                <td>2435321</td>
+                                <td>2435321</td>
+                                <td>2435321</td>
+                                <td>2435321</td>
+                            </tr>
+                            <tr>
+                                <td>00123</td>
+                                <td>Adidas Stan Smith</td>
+                                <td>12</td>
+                                <td>229900</td>
+                                <td>2435321</td>
+                                <td>2435321</td>
+                                <td>2435321</td>
+                                <td>2435321</td>
+                            </tr>
+                            <tr>
+                                <td>00123</td>
+                                <td>Adidas Stan Smith</td>
+                                <td>12</td>
+                                <td>229900</td>
+                                <td>2435321</td>
+                                <td>2435321</td>
+                                <td>2435321</td>
+                                <td>2435321</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
             {/*Contenedor de la tabla de Buscar y actualizar venta*/}
             {/* <div className="container">
@@ -55,7 +85,7 @@ const BuscarActualizarVentas = () => {
             </div> */}
             
             {/*Botón Para regresar*/}
-            <div className='flex justify-end mx-2'>
+            <div className='flex justify-end mx-2 h-auto'>
                 <Link to="/Ventas">
                         <button
                         className='py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-novablue hover:bg-gray-300 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500'
--- src/pages/gestionProducto.jsx
+++ src/pages/gestionProducto.jsx
@@ -7,42 +7,42 @@ const gestionProducto = () => {
     return (
         <div className="w-full h-full">
             {/*Título*/}
-            <div className='flex flex-col w-full mt-2'>
-                <h2 className='text-3xl font-extrabold text-gray-900 text-center'>
+            <div className='flex items-center justify-center w-full h-1/6'>
+                <h2 className='text-4xl font-extrabold text-gray-900'>
                 GESTIÓN DE PRODUCTOS
                 </h2>
             </div>
-
-            {/*Barras de búsqueda para la tabla*/}
-            <div className='flex flex-row items-center justify-center w-full mt-20'>
+            {/*Barras de búsqueda para la tabla */}
+            <div className='flex items-center justify-center w-full h-auto'>
                 <input
                 placeholder='Búsqueda por Nombre'
-                className='border-2 border-novablue mx-2 px-3 py-1 self-start rounded-md focus:outline-none focus:border-gray-500'
+                className='border-2 border-novablue mx-2 px-3 py-1 rounded-md focus:outline-none focus:border-gray-500'
                 />
                 <input
                 placeholder='Búsqueda por ID'
-                className='border-2 border-novablue mx-2 px-3 py-1 self-start rounded-md focus:outline-none focus:border-gray-500'
+                className='border-2 border-novablue mx-2 px-3 py-1 rounded-md focus:outline-none focus:border-gray-500'
                 />
             </div>
 
             {/* TABLA DE PRODUCTOS*/}
             <div className='hidden md:flex w-full mt-12'>
-                <table className="tabla">
-                <thead>
-                    <tr>
-                    <th className="text-center">ID Producto</th>
-                    <th className="text-center">Descripción</th>
-                    <th className="text-center">Valor Unitario</th>
-                    <th className="text-center">Estado</th>
-                    <th className="text-center">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <DataProducto idproducto="010010" descripcion="Adidas Stan Smith" valorUnitario="229900"/>
-                </tbody>
-                </table>
+                    <table class="tabla">
+                        <thead>
+                            <tr>
+                                <th className="text-center">ID Producto</th>
+                                <th className="text-center">Descripción</th>
+                                <th className="text-center">Valor Unitario</th>
+                                <th className="text-center">Estado</th>
+                                <th className="text-center">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <DataProducto idproducto="010010" descripcion="Adidas Stan Smith" valorUnitario="229900"/>
+                            <DataProducto idproducto="010010" descripcion="Adidas Stan Smith" valorUnitario="229900"/>
+                            <DataProducto idproducto="010010" descripcion="Adidas Stan Smith" valorUnitario="229900"/>
+                        </tbody>
+                    </table>
             </div>
-            
             {/*Botón Para regresar*/}
             <div className='flex justify-end mx-2'>
                 <Link to="/RegistroProducto">

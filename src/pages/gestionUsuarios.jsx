107ce88560a37b1f72708c9b4e98b16ce5f3cceb
Daniela Ovalle <daniovape.com>
2021-10-10 12:09:27 -0500

    Se arregla tabla de gestión de usuarios

--- src/pages/gestionUsuarios.jsx
+++ src/pages/gestionUsuarios.jsx
@@ -2,6 +2,8 @@ import Tabla from '../components/tablaUsuarios';
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../styles/usuarios.css';
 
+
+
 const  Usuarios = () => {
     return(
         <div className='w-full h-full'>
@@ -21,11 +23,14 @@ const  Usuarios = () => {
                 placeholder='Búsqueda por e-mail'
                 className='border-2 border-novablue mx-2 px-3 py-1 rounded-md focus:outline-none focus:border-gray-500'
                 />
+                <button className=' bg-novablue rounded border-novablue border-2 p-1 focus:outline-none focus:border-gray-500 text-white'>
+                  Buscar
+                </button>
             </div>
           {/* Tabla */}
           <div className='hidden md:flex w-full mt-12'>
             <table className="tabla">
-              <thead>
+              <thead className="text-center">
                 <tr>
                   <th>Nombre</th>
                   <th>Correo electrónico</th>
@@ -35,14 +40,36 @@ const  Usuarios = () => {
                 </tr>
               </thead>
               <tbody>
-                <Tabla correo="luisforero12@gmail.com" nombre="Luis Forero"/>
-                <Tabla correo="carodiaz17@gmail.com" nombre="Carolina Díaz"/>
-                <Tabla correo="shr_1982@gmail.com" nombre="Alberto Vélez "/>
-                <Tabla correo="saranghe@gmail.com" nombre="Cielo López"/>
-              </tbody>
-            </table>
-          </div>
+              <tr className="text-center">
+               <td >Luis Forero</td>
+               <td>luisforero@gmail.com</td>
+               <td className="text-center">
+               <select className="rounded-lg m-2 w-auto">
+               <option disabled>Rol</option>
+               <option>Vendedor</option>
+               <option>Administrador</option>
+               </select>
+              </td>
+              <td className="text-center">
+              <select className="rounded-lg m-2 w-auto">
+              <option disabled>Estado</option>
+              <option>Pendiente</option>
+              <option>Aceptado</option>
+              <option>Rechazado</option>
+              </select>  
+             </td>
+            <td className='text-center'>
+            <div className="flex justify-around">
+            <button className="rounded border-solid border-2 border-novablue far fa-edit bg-info"></button>
+            <button className="rounded border-solid border-2 border-red-600 far fa-trash-alt bg-danger"></button>
+            </div>
+            </td>
+            </tr>
+           </tbody>
+          </table>
         </div>
+      </div>
           )
 }
 export default Usuarios;
+

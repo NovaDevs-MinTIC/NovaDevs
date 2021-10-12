f5f10750532509bc17ccca7fc7e7f7cc46f55743
Oscar <os.gomez.lozano@gmail.com>
2021-10-12 18:05:10 -0500

    resolviendo yarn.lock

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ImagenLogoNegro from 'components/ImagenLogoNegro';
-import { useAuth0 } from "@auth0/auth0-react";
+/* import { useAuth0 } from "@auth0/auth0-react"; */
 
 const Login = () => {
     return (
